43a6b627938217ab8c93eb61c2de6476624d5c43
Shawkath_Muhammed <shawkathvimuhammed@gmail.com>
2025-12-31 15:19:52 +0530

    Add files via upload

---
+++ client_exp3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#define SERVER_PORT 12345
+#define BUFFER_SIZE 1024
+int main(){
+      int sockfd;
+      struct sockaddr_in server_addr;
+      char buffer[BUFFER_SIZE];
+      int num1,num2;
+      sockfd = socket(AF_INET,SOCK_DGRAM,0);
+      if(sockfd < 0){
+         perror("Socket creation failed");
+         exit(EXIT_FAILURE);
+      }
+      memset(&server_addr,0,sizeof(server_addr));
+      server_addr.sin_family = AF_INET;
+      server_addr.sin_port = htons(SERVER_PORT);
+      server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+      printf("Enter the first number:");
+      scanf("%d",&num1);
+      printf("Enter the second number:");
+      scanf("%d",&num2);
+      snprintf(buffer,sizeof(buffer),"%d %d",num1,num2);
+      if(sendto(sockfd,buffer,strlen(buffer),0,(struct sockaddr *)&server_addr,sizeof(server_addr)) < 0 ){
+         perror("Sendto failed");
+         close(sockfd);
+         exit(EXIT_FAILURE);
+      }
+      int recv_len = recvfrom(sockfd,buffer,BUFFER_SIZE,0,NULL,NULL);
+      if(recv_len < 0){
+        perror("Recvfrom failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+      }
+      buffer[recv_len] = '\0';
+      close(sockfd);
+      return 0;
+   }

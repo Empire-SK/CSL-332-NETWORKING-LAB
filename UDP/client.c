09c56a8d324f7129d7685a41ade787c41bf83bb2
Empire <sabinksanthosh6@gmail.com>
2026-05-17 23:55:05 +0530

    spacing

--- UDP/client.c
+++ UDP/client.c
@@ -11,27 +11,32 @@ int main()
     struct sockaddr_in server_addr;
     char buffer[BUFFER_SIZE];
     int num1, num2;
+
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0)
     {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
+
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
     server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
     printf("Enter first number: ");
     scanf("%d", &num1);
     printf("Enter second number: ");
     scanf("%d", &num2);
     snprintf(buffer, sizeof(buffer), "%d %d", num1, num2);
+
     if (sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         perror("sendto failed");
         close(sockfd);
         exit(EXIT_FAILURE);
     }
+    
     int recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
     if (recv_len < 0)
     {
--- UDP/server.c
+++ UDP/server.c
@@ -12,16 +12,19 @@ int main()
     socklen_t addr_len = sizeof(client_addr);
     char buffer[BUFFER_SIZE];
     int num1, num2, result;
+
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0)
     {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
+
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(SERVER_PORT);
+
     if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         perror("Bind failed");
@@ -29,6 +32,7 @@ int main()
         exit(EXIT_FAILURE);
     }
     printf("server listening on port %d..\n", SERVER_PORT);
+    
     while (1)
     {
         int recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (structsockaddr *)&client_addr, &addr_len);

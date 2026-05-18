81dfa405eac6868ea5f363ce06e0e994d1a7e447
Empire <sabinksanthosh6@gmail.com>
2026-05-18 13:41:34 +0530

    Add SMTP client and server implementation for email communication

--- SMTP/server.c
+++ SMTP/server.c
@@ -4,13 +4,18 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <unistd.h>
-void check(char strr[], int value){
-  if (value < 0){
+
+void check(char strr[], int value)
+{
+  if (value < 0)
+  {
     perror(strr);
     exit(1);
   }
-  }
-int main(){
+}
+
+int main()
+{
   int sid, sbind, slisten, saccept, srec, ssend, size;
   char recbuff[500], sendbuff[500], fromaddress[500], toaddress[500], mailbody[1000];
   struct sockaddr_in saddr, caddr;
@@ -25,7 +30,7 @@ int main(){
   slisten = listen(sid, 5);
   check("LISTEN", slisten);
   printf("Server listening on port 8082...\n");
-  saccept = accept(sid, (struct sockaddr *)&caddr, (socklen_t*)&size);
+  saccept = accept(sid, (struct sockaddr *)&caddr, (socklen_t *)&size);
   check("ACCEPT", saccept);
   printf("\nClient Connected.\n");
 
@@ -40,7 +45,7 @@ int main(){
   printf("Waiting for client response..\n");
   srec = recv(saccept, recbuff, 500, 0);
   check("RECEIVE", srec);
-  if(strncmp(recbuff, "HELLO", 5) != 0)
+  if (strncmp(recbuff, "HELLO", 5) != 0)
     printf("Error: Expected HELLO\n");
   else
     printf("Message from client: %s\n", recbuff);
@@ -63,45 +68,48 @@ int main(){
   printf("Waiting for TO address\n");
   srec = recv(saccept, toaddress, 500, 0);
   check("RECEIVE", srec);
-  if(strncmp(toaddress, "MAIL TO:", 8) != 0)
+  if (strncmp(toaddress, "MAIL TO:", 8) != 0)
     printf("To address error\n");
   else
-    printf("To address: %s\n", toaddress);  
+    printf("To address: %s\n", toaddress);
   printf("Sending OK to TO address\n");
   strcpy(sendbuff, "250 OK");
   ssend = send(saccept, sendbuff, 500, 0);
   check("SENDING", ssend);
 
   printf("Waiting for DATA\n");
-  srec = recv(saccept, recbuff, 500, 0);  
+  srec = recv(saccept, recbuff, 500, 0);
   check("RECEIVE", srec);
-  if(strncmp(recbuff, "DATA", 4) != 0)
+  if (strncmp(recbuff, "DATA", 4) != 0)
     printf("Data not received\n");
   else
-  printf("Message from client: %s\n", recbuff);
+    printf("Message from client: %s\n", recbuff);
   printf("Sending GO AHEAD to client\n");
   strcpy(sendbuff, "354 GO AHEAD");
   ssend = send(saccept, sendbuff, 500, 0);
   check("SENDING", ssend);
 
   printf("Mail body:\n");
-  while (1){
+  while (1)
+  {
     srec = recv(saccept, mailbody, 1000, 0);
     check("MAIL BODY RECEIVE", srec);
-  if(strncmp(mailbody, "$", 1) == 0)
-    break;
-  else
-    printf("%s", mailbody);}
+    if (strncmp(mailbody, "$", 1) == 0)
+      break;
+    else
+      printf("%s", mailbody);
+  }
   strcpy(sendbuff, "221 OK");
   ssend = send(saccept, sendbuff, 500, 0);
   printf("Sending OK to client (Body received)\n");
   check("SENDING", ssend);
 
   srec = recv(saccept, mailbody, 1000, 0);
-  if(strncmp(mailbody, "QUIT", 4) == 0){
-  strcpy(sendbuff, "221 OK");
-  ssend = send(saccept, sendbuff, 500, 0);
-  printf("Sending 221 OK for QUIT...\n");
+  if (strncmp(mailbody, "QUIT", 4) == 0)
+  {
+    strcpy(sendbuff, "221 OK");
+    ssend = send(saccept, sendbuff, 500, 0);
+    printf("Sending 221 OK for QUIT...\n");
   }
   printf("Connection closed\n");
   close(saccept);
